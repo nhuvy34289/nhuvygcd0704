49bb9b8695cb87e2e787dca3d3c411f9723e106b
DELL <DELL@DESKTOP-FPVTJLF>
2020-06-26 11:17:33 +0700

    commit part 1

--- DeleteData.php
+++ DeleteData.php
@@ -28,7 +28,7 @@ if (empty(getenv("DATABASE_URL"))){
    ));
 }  
 
-$sql = "DELETE FROM customers WHERE CustomerID = '2'";
+$sql = "DELETE FROM customers WHERE  = '2'";
 $stmt = $pdo->prepare($sql);
 if($stmt->execute() == TRUE){
     echo "Record deleted successfully.";
